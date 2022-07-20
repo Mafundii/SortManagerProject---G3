5eed02c5b0d108502a61f0dc0b3af910dceb1e12
Mafundii <makshadys1@gmail.com>
2022-07-20 14:41:33 +0100

    Initial solution

---
+++ SortManager/SortManagerApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SortManagerApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
