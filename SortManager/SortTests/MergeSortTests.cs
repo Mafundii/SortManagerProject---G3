7a35b1f73bff40f68de802269507b3998dbf9db1
Kenny <kharvey@spartaglobal.com>
2022-07-21 11:36:50 +0100

    Test Project created. TestData class used for all test data to run in each of the test classes

---
+++ SortManager/SortTests/MergeSortTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SortTests
+{
+    public class MergeSortTests
+    {
+        [TestCaseSource(typeof(TestData), nameof(TestData.ArraysToTest))]
+        public void GivenAnArrayOfMultipeDifferentNumbers_Sort_ReturnsLowestToHighest(int[] array, int[] resultArray)
+        {
+            Assert.That(MergeSort.Sort(array), Is.EqualTo(resultArray));
+        }
+
+        [TestCaseSource(typeof(TestData), nameof(TestData.NegativeArraysToTest))]
+        public void GivenAnArrayOfNegativeNumbers_Sort_ReturnsLowestToHighest(int[] array, int[] resultArray)
+        {
+            Assert.That(MergeSort.Sort(array), Is.EqualTo(resultArray));
+        }
+
+        [TestCaseSource(typeof(TestData), nameof(TestData.OneElementArrayToTest))]
+        public void GivenAnArrayOfOneNumber_Sort_ReturnsSameNumber(int[] array)
+        {
+            Assert.That(MergeSort.Sort(array), Is.EqualTo(array));
+        }
+
+        [TestCaseSource(typeof(TestData), nameof(TestData.EmptyArrayToTest))]
+        public void GivenAnEmptyArray_Sort_ReturnsEmptyArray(int[] array)
+        {
+            Assert.That(MergeSort.Sort(array), Is.EqualTo(array));
+        }
+
+        [TestCaseSource(typeof(TestData), nameof(TestData.SameNumberArrayToTest))]
+        public void GivenAnArrayOfTheSameNumbers_Sort_ReturnsSameNumbers(int[] array)
+        {
+            Assert.That(MergeSort.Sort(array), Is.EqualTo(array));
+        }
+    }
+}
