18d5b709a8d077fec3cc6b96d98990c4c118efd4
SupaDylan11 <91839242+SupaDylan11@users.noreply.github.com>
2022-07-21 14:27:50 +0100

    Bubble sort completed with all tests passing

---
+++ SortManager/SortAlgorithms/BubbleSort.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace SortAlgorithms
+{
+    public class BubbleSort
+    {
+        public static int[] BubbleSorting(int[] intArray)
+        {
+            if (intArray.Length == 0 || intArray == null)
+                return intArray;
+
+            for (int j = 0; j < intArray.Length - 1; j++)
+            {
+                for (int i = 0; i < intArray.Length - 1; i++)
+                {
+                    if (intArray[i] > intArray[i + 1])
+                    {
+                        (intArray[i + 1], intArray[i]) = (intArray[i], intArray[i + 1]); // hold the larger vakue in the temp holder
+                    }
+                }
+            }
+            return intArray;
+        }
+    }
+}
+
