d813bb39abe7e477d5872feeb5010afd1da02cf6
pkaichan <punkai.chan@gmail.com>
2022-07-22 09:45:56 +0100

    option to sort the same array again

--- SortManager/Controller/Controller.cs
+++ SortManager/Controller/Controller.cs
@@ -5,6 +5,8 @@ namespace SortManagerApp
     public class Controller
     {
         public static string SortMethod { get; set; }
+        public static int[] ArrayGenerated { get; set; }
+
         public static int ParseOption(string? option)
         {
             if (option == null) return 0;
@@ -25,32 +27,46 @@ namespace SortManagerApp
             }
             return num;
         }
+
+        public static int ParseOption2(string? option)
+        {
+            if (option == null) return 0;
+            if (Int32.TryParse(option, out int num))
+            {
+                if (num < 1 || num > 5)
+                    return 0;
+            }
+            return num;
+        }
         public static int[] GenerateArray(int len)
         {
-            var _unsortedArray = new int[len];
+            ArrayGenerated = new int[len];
 
             var random = new Random();
             for (int i = 0; i < len; i++)
-                _unsortedArray[i] = random.Next(0, 1000);
-            return _unsortedArray;
+                ArrayGenerated[i] = random.Next(0, 1000);
+            return ArrayGenerated;
         }
         public static int[] SortArray(int[] array, int option)
         {
+            var newArray = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+                newArray[i] = array[i];
             switch (option)
             {
                 case 1:
                     SortMethod = SortAlgorithms.BubbleSort.Name;
-                    return SortAlgorithms.BubbleSort.BubbleSorting(array);
+                    return SortAlgorithms.BubbleSort.BubbleSorting(newArray);
                 case 2:
                     SortMethod = SortAlgorithms.MergeSort.Name;
-                    return SortAlgorithms.MergeSort.MergeSorting(array);
+                    return SortAlgorithms.MergeSort.MergeSorting(newArray);
                 case 3:
                     SortMethod = SortAlgorithms.NetSort.Name;
-                    return SortAlgorithms.NetSort.NetSorting(array);
+                    return SortAlgorithms.NetSort.NetSorting(newArray);
                 default:
-                    return array;
+                    return newArray;
             }
-        }
+        }  
         public static string ArrayString(int[] sortedArray)
         {
             return String.Join(", ", sortedArray);
--- SortManager/SortManagerApp/Program.cs
+++ SortManager/SortManagerApp/Program.cs
@@ -37,8 +37,25 @@ namespace SortManagerApp
                     len = Console.ReadLine();
                     parsedLen = Controller.ParseLen(len);
                 }
-                
                 DisplaySorted(parsedOption, parsedLen);
+
+                while (true)
+                {
+                    SortAgain();
+                    var option2 = Console.ReadLine();
+                    var parsedOption2 = Controller.ParseOption2(option2);
+                    while (parsedOption2 == 0)
+                    {
+                        InvalidInput();
+                        SortAgain();
+                        option2 = Console.ReadLine();
+                        parsedOption2 = Controller.ParseOption2(option2);
+                    }
+                    if (parsedOption2 == 4) break;
+                    else if (parsedOption2 == 5) return;
+                    else DisplaySorted(parsedOption2);
+                }
+                Console.WriteLine($"\n{Seperator}\n");
             }
         }
         public static void MainMenu()
@@ -57,6 +74,15 @@ namespace SortManagerApp
         {
             Console.WriteLine($"\n{Seperator}\n\nPlease enter the size of the array from 0 to 20:\n");
         }
+        public static void SortAgain()
+        {
+            Console.WriteLine("Sort the same array again:\n" +
+                "[1] Bubble Sort\n" +
+                "[2] Merge Sort\n" +
+                "[3] .NET Sort\n" +
+                "[4] Sort a New Array\n" +
+                "[5] Exit\n");
+        }
         public static void DisplaySorted(int option, int len)
         {
             var array = Controller.GenerateArray(len);
@@ -73,7 +99,24 @@ namespace SortManagerApp
 
             Console.WriteLine($"Sorted array: {Controller.ArrayString(sortedArray)}");
             Console.WriteLine($"Sort method: {Controller.SortMethod}");
-            Console.WriteLine($"Time taken: {Math.Round(stopwatch.Elapsed.TotalMilliseconds,1)} ms\n\n{Seperator}\n");
+            Console.WriteLine($"Time taken: {Math.Round(stopwatch.Elapsed.TotalMilliseconds,5)} ms\n");
+        }
+
+        public static void DisplaySorted(int option)
+        {
+            Console.WriteLine($"\n{Seperator}\n");
+            Console.WriteLine($"Array generated: {Controller.ArrayString(Controller.ArrayGenerated)}");
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            var sortedArray = Controller.SortArray(Controller.ArrayGenerated, option);
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"Sorted array: {Controller.ArrayString(sortedArray)}");
+            Console.WriteLine($"Sort method: {Controller.SortMethod}");
+            Console.WriteLine($"Time taken: {Math.Round(stopwatch.Elapsed.TotalMilliseconds, 5)} ms\n");
         }
     }
 }
